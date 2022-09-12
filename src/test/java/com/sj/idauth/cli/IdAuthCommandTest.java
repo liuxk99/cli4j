2c42d48efe6be2253c2185fa00e3d850daa7fab8
Thomas Liu <liuxk99@gmail.com>
2022-09-12 19:29:14 +0800

    <Test>: Add testcases for coverage.

--- src/test/java/com/sj/idauth/cli/IdAuthCommandTest.java
+++ src/test/java/com/sj/idauth/cli/IdAuthCommandTest.java
@@ -28,6 +28,26 @@ class IdAuthCommandTest {
     }
 
     @Test
+    void should_login_with_null_user() {
+        // 1
+        cli.execute("login", "-u=", "-p=");
+//        Missing required options: '--user=<arg0>', '--password=<arg1>'
+//        Usage: IdAuth login -p=<arg1> -u=<arg0>
+//                -p, --password=<arg1>   Password for user to login.
+//                -u, --user=<arg0>       User name to login.
+    }
+
+    @Test
+    void should_login_with_null_password() {
+        // 1
+        cli.execute("login", "-u=aaa", "-p=");
+//        Missing required options: '--user=<arg0>', '--password=<arg1>'
+//        Usage: IdAuth login -p=<arg1> -u=<arg0>
+//                -p, --password=<arg1>   Password for user to login.
+//                -u, --user=<arg0>       User name to login.
+    }
+
+    @Test
     public void should_login_with_parameters() {
         final String user = System.getenv("MY_USER");
         final String password = System.getenv("MY_PASSWORD");
